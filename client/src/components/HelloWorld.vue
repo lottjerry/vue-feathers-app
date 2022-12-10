cbdb5683cbdc1075a80fa471a786b9dcb0a1f7d5
Jerry Lott <jj@Jerrys-Mac-mini.local>
2022-12-09 23:52:47 -0600

    commit client

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-green-700 text-5xl">Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
