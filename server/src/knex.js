a5300cf517cca0ba029a840da91be64ccea8f757
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-01-02 15:57:51 -0600

    Reset setup

--- server/src/knex.js
+++ server/src/knex.js
@@ -1,16 +1,8 @@
-const knex = require("knex");
+const knex = require('knex');
 
 module.exports = function (app) {
-  const db = require("knex")({
-    client: "mysql2",
-    connection: {
-      host: "localhost",
-      port: 3306,
-      user: "mysql-username",
-      password: "mysql-password",
-      database: "database-name",
-    },
-  });
+  const { client, connection } = app.get('mysql');
+  const db = knex({ client, connection });
 
-  app.set("knexClient", db);
+  app.set('knexClient', db);
 };
