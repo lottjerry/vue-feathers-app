a25048d6b9d7940fc4b0a3f638fff3c5ccc63206
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-01-16 17:06:25 -0600

    Added functionality for when if a user has already authenticated (jwt already in browser) automatically redirect user to tasks page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,30 +1,36 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import LoginView from "../views/LoginView.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import LoginView from '../views/LoginView.vue';
+import TasksView from '../views/TasksView.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "login",
-    component: LoginView,
-  },
-  {
-    path: "/signup",
-    name: "signup",
-    // route level code-splitting
-    // this generates a separate chunk (signup.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "signup" */ "../views/SignupView.vue"),
-  },
+	{
+		path: '/',
+		name: 'login',
+		component: LoginView,
+	},
+	{
+		path: '/signup',
+		name: 'signup',
+		// route level code-splitting
+		// this generates a separate chunk (signup.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "signup" */ '../views/SignupView.vue'),
+	},
+	{
+		path: '/tasks',
+		name: 'tasks',
+		component: TasksView,
+	},
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
 });
 
 export default router;
