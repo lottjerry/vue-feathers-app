16a9833f805abb8311bb116eda97bc0cbcfb11aa
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-02-08 01:31:46 -0600

    Fixed problem with messages service showing empty object in the feathers-vuex store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,7 @@ import feathersVuex from 'feathers-vuex';
 import feathersClient from '../fathers-client';
 
 const { service, auth, FeathersVuex } = feathersVuex(feathersClient, {
-	idField: '_id',
+	idField: 'id', /// OMFG the problem was i had to change idField: '_id' to 'id'
 });
 
 Vue.use(Vuex);
@@ -13,8 +13,12 @@ Vue.use(FeathersVuex);
 export default new Vuex.Store({
 	plugins: [
 		// Create user with the user service then call auth on that user
-		service('users'),
-		service('messages'),
+		service('users', {
+			//debug: true
+		}),
+		service('messages', {
+			//debug: true
+		}),
 		// Setup the auth plugin.
 		auth({ userService: 'users' }),
 	],
