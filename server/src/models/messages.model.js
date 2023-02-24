1e56767e6967434fbe92d505e849c27760f36557
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-02-24 13:59:46 -0600

    change the id table of the message model to be the primary key

--- server/src/models/messages.model.js
+++ server/src/models/messages.model.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
     if (!exists) {
       db.schema
         .createTable(tableName, (table) => {
-          table.increments("id");
+          table.increments("id").primary();
           table.integer("userId").unsigned();
           table.foreign("userId").references("users.id");
           table.string("messageBody");
