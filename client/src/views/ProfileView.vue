9f00d5501ed866b1c839fb474fd9278a3c06e46c
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-02-27 01:10:12 -0600

    User will automatically be loggedin on signup. Users can now update their profile information and they can also delete their account (Bug pertaining to foreign key constraint on account deletion when the users has messages)

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="flex justify-center">
 		<div
-			class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-sm w-full flex-col gap-4"
+			class="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4 max-w-lg w-full flex-col gap-4 border justify-center"
 		>
-				<h1 class="text-3xl font-bold text-blue-600 p-5 underline">My Profile</h1>
-			<div class="mb-6 p-3">
+			<h1 class="text-3xl font-bold text-blue-600 p-6 underline">My Profile</h1>
+			<div class="mb-6 p-3 flex justify-center">
 				<div class="relative flex gap-2" v-for="user in users" :key="user.id">
 					<div class="flex self-center">Username:</div>
 					<div v-if="!usernameEdit" class="mx-4 mr-16">
@@ -59,10 +59,10 @@
 					</svg>
 				</div>
 			</div>
-			<div class="mb-6 p-3">
-				<div class="relative flex gap-2" v-for="user in users" :key="user.id">
+			<div class="mb-6 flex justify-center">
+				<div class="relative flex gap-4" v-for="user in users" :key="user.id">
 					<div class="flex self-center">Email:</div>
-					<div v-if="!emailEdit" class="mx-4 ml-12">
+					<div v-if="!emailEdit" class="">
 						{{ user.email }}
 					</div>
 					<div class="relative ml-8" v-else>
@@ -80,8 +80,8 @@
 						>
 					</div>
 					<svg
-					v-if="!emailEdit"
-					@click="toggleEmailEdit()"
+						v-if="!emailEdit"
+						@click="toggleEmailEdit()"
 						xmlns="http://www.w3.org/2000/svg"
 						class="hover:stroke-blue-600 hover:cursor-pointer flex self-center"
 						width="24"
@@ -96,8 +96,8 @@
 						<polygon points="16 3 21 8 8 21 3 21 3 16 16 3"></polygon>
 					</svg>
 					<svg
-					v-else
-					@click="cancelEmailEdit()"
+						v-else
+						@click="cancelEmailEdit()"
 						xmlns="http://www.w3.org/2000/svg"
 						class="hover:stroke-red-600 hover:cursor-pointer flex self-center"
 						width="24"
@@ -114,6 +114,81 @@
 					</svg>
 				</div>
 			</div>
+			<!-- PROMPT DELTE -->
+			<div
+				class="flex justify-center p-5"
+				v-if="promptDelete && !(usernameEdit || emailEdit)"
+			>
+				<div class="flex flex-col gap-5" v-for="user in users" :key="user.id">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="stroke-red-600 self-center"
+						width="40"
+						height="40"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="#000000"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<polygon
+							points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"
+						></polygon>
+						<line x1="12" y1="8" x2="12" y2="12"></line>
+						<line x1="12" y1="16" x2="12.01" y2="16"></line>
+					</svg>
+					<h1 class="text-red-600 text-l">
+						Are you sure you want to delete your account ?
+					</h1>
+					<div class="flex gap-5 justify-center">
+						<button
+							@click="deleteUser(user.id)"
+							class="border-2 border-green-600 bg-white hover:bg-green-600 text-green-600 hover:text-white font-bold py-1 px-2 w-20 rounded focus:outline-none focus:shadow-outline transition ease-in-out"
+						>
+							Yes
+						</button>
+						<button
+							@click="cancelDelete()"
+							class="border-2 border-red-600 bg-white hover:bg-red-600 text-red-600 hover:text-white font-bold py-1 px-2 w-20 rounded focus:outline-none focus:shadow-outline transition ease-in-out"
+						>
+							No
+						</button>
+					</div>
+				</div>
+			</div>
+			<!-- END PROMPT DELETE -->
+			<div v-if="!promptDelete">
+				<button
+					v-if="!(usernameEdit || emailEdit)"
+					@click="deletePrompt()"
+					class="border-2 border-red-600 bg-white hover:bg-red-600 text-red-600 hover:text-white font-bold py-1 px-2 rounded hover:cursor-pointer focus:outline-none focus:shadow-outline transition ease-in-out mx-10 my-16"
+				>
+					Delete Account
+				</button>
+			</div>
+			<!-- User Update buttons -->
+			<div v-if="usernameEdit || emailEdit">
+				<div
+					class="flex justify-center gap-5 mx-5 my-16"
+					v-for="user in users"
+					:key="user.id"
+				>
+					<button
+						@click="updateUser(user.id)"
+						class="border-2 border-blue-600 bg-white hover:bg-blue-600 text-blue-600 hover:text-white font-bold py-1 px-2 rounded hover:cursor-pointer focus:outline-none focus:shadow-outline transition ease-in-out"
+					>
+						Update
+					</button>
+					<button
+						@click="cancelUpdate()"
+						class="border-2 border-red-600 bg-white hover:bg-red-600 text-red-600 hover:text-white font-bold py-1 px-2 rounded hover:cursor-pointer focus:outline-none focus:shadow-outline transition ease-in-out"
+					>
+						Cancel
+					</button>
+				</div>
+			</div>
+			<!-- End User Update Buttons -->
 		</div>
 	</div>
 </template>
@@ -128,6 +203,7 @@ export default {
 		newEmail: '',
 		usernameEdit: false,
 		emailEdit: false,
+		promptDelete: false,
 	}),
 	mounted() {
 		this.findUsers({ query: {} });
@@ -137,22 +213,37 @@ export default {
 		deleteUser(userId) {
 			const { User } = this.$FeathersVuex;
 			const user = new User({ id: userId });
-			user.remove();
+			user
+				.remove()
+				.then(() => {
+					this.$router.go('/');
+					window.setTimeout(function(){
+                alert("Account Successfully Deleted!");
+            }, 3000); 
+				})
 		},
 		updateUser(userId) {
 			const { User } = this.$FeathersVuex;
 			const user = new User({ id: userId });
-			user.username = this.newUsername;
+			if (this.newUsername != '') {
+				user.username = this.newUsername;
+			}
+			if (this.newEmail != '') {
+				user.email = this.newEmail;
+			}
 			user.save().then(() => {
-				this.selected = '';
-				this.newUsername = '';
+				this.cancelUpdate();
+				alert('Account updated Successfully!'
+				)
 			});
 		},
 		toggleUsernameEdit() {
 			this.usernameEdit = true;
+			this.promptDelete = false;
 		},
 		toggleEmailEdit() {
 			this.emailEdit = true;
+			this.promptDelete = false;
 		},
 		cancelUsernameEdit() {
 			this.newUsername = '';
@@ -161,6 +252,16 @@ export default {
 		cancelEmailEdit() {
 			this.newEmail = '';
 			this.emailEdit = false;
+		},
+		deletePrompt() {
+			this.promptDelete = true;
+		},
+		cancelDelete() {
+			this.promptDelete = false;
+		},
+		cancelUpdate() {
+			this.cancelUsernameEdit();
+			this.cancelEmailEdit();
 		},
 	},
 	computed: {
