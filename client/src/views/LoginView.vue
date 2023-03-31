33a801c3c296ab7282128e576d7477613c595012
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-03-31 00:06:29 -0500

    Added FloatingInput component

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -7,65 +7,29 @@
 					@submit.prevent="login"
 					class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
 				>
-					<div class="mb-6">
-						<div class="relative">
-							<div class="relative">
-								<input
-									v-model="user.username"
-									type="text"
-									id="username"
-									class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-									placeholder=" "
-								/>
-								<label
-									for="username"
-									class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-									>Username</label
-								>
-							</div>
-						</div>
+					<FloatingInput v-model="user.username" label="Username" type="text"/>
+					<FloatingInput v-model="user.password" label="Password" type="password"/>
+					<div v-if="errorMessage" class="flex gap-2 justify-center mb-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="stroke-red-600"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="#000000"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<polygon
+								points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"
+							></polygon>
+							<line x1="12" y1="8" x2="12" y2="12"></line>
+							<line x1="12" y1="16" x2="12.01" y2="16"></line>
+						</svg>
+						<p class="text-red-600">Invalid Username or Email.</p>
 					</div>
-					<div class="mb-6">
-						<div class="relative">
-							<div class="relative">
-								<input
-									v-model="user.password"
-									type="password"
-									id="password"
-									class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-									placeholder=" "
-								/>
-								<label
-									for="password"
-									class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-									>Password</label
-								>
-							</div>
-						</div>
-					</div>
-					<div 
-					v-if="errorMessage"
-					class="flex gap-2 justify-center mb-4">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="stroke-red-600"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="#000000"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							>
-								<polygon
-									points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"
-								></polygon>
-								<line x1="12" y1="8" x2="12" y2="12"></line>
-								<line x1="12" y1="16" x2="12.01" y2="16"></line>
-							</svg>
-							<p class="text-red-600">Invalid Username or Email.</p>
-						</div>
 					<div class="flex-col items-center">
 						<button
 							class="border-2 border-blue-600 bg-white hover:bg-blue-600 text-blue-600 hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition ease-in-out"
@@ -93,8 +57,12 @@
 
 <script>
 import { mapActions } from 'vuex';
+import FloatingInput from '../components/FloatingInput.vue';
 
 export default {
+	components: {
+		FloatingInput,
+	},
 	name: 'LoginView',
 	data: () => ({
 		user: {
@@ -118,6 +86,7 @@ export default {
 					// Show login page (potentially with `e.message`)
 					console.error('Authentication error', error);
 					this.errorMessage = true;
+					console.log();
 				});
 		},
 	},
