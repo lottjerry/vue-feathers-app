b5cb9d3491b87d7ccef721575f9cbc7687b2019f
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-04-10 01:08:07 -0500

    Added process-message hook to handle message input to the database

---
+++ server/src/hooks/process-message.js
@@ -0,0 +1,32 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (date, time) => {
+  return async (context) => {
+    // data from message
+    const { data } = context;
+
+    // Throw an error if database did not recieve a message
+    if (!data.messageBody) {
+      throw new Error("A message can not be empty");
+    }
+
+    // The logged in user
+    const { user } = context.params;
+    // The actual message text
+    // Make sure that messages are no longer than 400 characters
+    const text = data.messageBody.substring(0, 400);
+
+    // Update the original data (so that people can't submit additional stuff)
+    data.messageBody = text;
+    data.userId = user.id;
+    data[date] = new Date().toLocaleDateString("en-us", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+    data[time] = new Date().toLocaleTimeString("en-US");
+    return context;
+  };
+};
