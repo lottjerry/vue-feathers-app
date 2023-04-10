6cf7d0ecd4ad3867db684073cefa65a395b44bde
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-04-10 01:08:07 -0500

    Added process-message hook to handle message input to the database

--- server/src/services/messages/messages.hooks.js
+++ server/src/services/messages/messages.hooks.js
@@ -1,20 +1,12 @@
 const { authenticate } = require("@feathersjs/authentication").hooks;
 const { setField } = require("feathers-authentication-hooks");
 
-// Set the current user id as userId when creating a message and only allow users to edit and remove their own messages:
-
-const setUserId = setField({
-  from: "params.user.id",
-  as: "data.userId",
-});
 const limitToUser = setField({
   from: "params.user.id",
   as: "params.query.userId",
 });
 
-
-const setDateStamp = require('../../hooks/set-date-stamp');
-const setTimeStamp = require('../../hooks/set-time-stamp');
+const processMessage = require('../../hooks/process-message');
 
 
 module.exports = {
@@ -22,8 +14,13 @@ module.exports = {
     all: [authenticate("jwt")],
     find: [limitToUser],
     get: [limitToUser],
-    create: [setUserId, setDateStamp('dateCreated'), setTimeStamp('timeCreated')],
-    update: [limitToUser, setDateStamp('dateUpdated'), setTimeStamp('timeUpdated')],
+    create: [
+      processMessage('dateCreated','timeCreated')
+    ],
+    update: [
+      limitToUser,
+      processMessage('dateUpdated','timeUpdated')
+    ],
     patch: [limitToUser],
     remove: [limitToUser],
   },
